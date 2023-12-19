f902cff0160930361b62ac9faa6a5ee2e7bb4c0f
Dyanna Turner <dttheme@gmail.com>
2023-12-19 13:26:57 -0500

    Cleanup and deploy

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./index.css"
+import "./components"
+import {Header, Card} from "./components";
+
+export default () => (
+
+<div className="app-wrapper">
+  <Header>
+    <h1>Peach Butter Prints</h1>
+  </Header>
+  <Card>
+    <p>Artist and software engineer living in Atlanta</p>
+  </Card>
+  <Card>
+    <p>This website is under contstruction!</p>
+  </Card>
+
+
+
+
+
+
+
+
+
+
+
+    {/* links */}
+    {/* hero image */}
+    {/* about me */}
+    {/* gallery/favs*/}
+    {/* commission information */}
+    {/* contact form */}
+
+
+
+
+
+
+
+
+
+
+
+    {/* ideas:
+    website changes when they select different image
+    */}
+</div>
+);
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../index.css";
+
+export default ({children}) => {
+    return (<div className="header">
+        {children}
+    </div>)
+}

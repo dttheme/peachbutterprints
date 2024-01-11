53b7c51f0d0b5b4c20f47426bf45eaaec40cf98b
Dyanna Turner <dttheme@gmail.com>
2024-01-11 14:49:40 -0500

    add fb trigger email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,62 @@
-import React, { useState } from "react"
-import "../index.css"
+import React, { useState } from "react";
+import "../index.css";
 
-import {postContact} from "../firestore"
-
-
+import { postContact, sendEmail } from "../firestore";
 
 export default () => {
-    const [name, updateName] = useState('')
-    const [email, updateEmail] = useState('')
-    const [message, updateMessage] = useState('')
+  const [name, updateName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [message, updateMessage] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        postContact({name, email, message});
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    
-    
-    return (
-        <div className="contact">
-            <form onSubmit={handleSubmit}>
-            <h2><b>Contact</b></h2>
-            <div>Interested in some custom art? Have questions? Please reach out using the form below, or directly at <a href="mailto:peachbutterprints@gmail.com">peachbutterprints@gmail.com</a></div>
-                <label htmlFor="">Name</label>
-                <input type="text" placeholder="Name" value={name} onChange={(e)=>updateName(e.target.value)} required/>
-                <label htmlFor="">Email</label>
-                <input type="email" placeholder="Email" value={email} onChange={(e)=> updateEmail(e.target.value)} required/>
-                <label htmlFor="">Message</label>
-                <textarea rows="10" placeholder="Message" value={message} onChange={(e)=> updateMessage(e.target.value)} required></textarea>
-                <button type="sumbit">
-                    Submit
-                </button>
-            </form>
+    postContact({ name, email, message })
+      .then((resp) => {
+        return sendEmail({ name, email, message });
+      })
+      .then(() => console.log("Queued email for delivery!"));
+  };
+
+  return (
+    <div className="contact">
+      <form onSubmit={handleSubmit}>
+        <h2>
+          <b>Contact</b>
+        </h2>
+        <div>
+          Interested in some custom art? Have questions? Please reach out using
+          the form below, or directly at{" "}
+          <a href="mailto:peachbutterprints@gmail.com">
+            peachbutterprints@gmail.com
+          </a>
         </div>
-    )
-}
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => updateName(e.target.value)}
+          required
+        />
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => updateEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="">Message</label>
+        <textarea
+          rows="10"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => updateMessage(e.target.value)}
+          required
+        ></textarea>
+        <button type="sumbit">Submit</button>
+      </form>
+    </div>
+  );
+};
